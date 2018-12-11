03c92af61874c15368e2b33178c05a85b26d5410
Gabriel <gabriel-araujo52@hotmail.com>
2018-12-10 23:40:50 -0300

    passando menssagens corretamente

--- mochila_linhas.c
+++ mochila_linhas.c
@@ -16,63 +16,42 @@ void parser(int* valor, int* peso){
 	int i, j;
 	FILE *pont_arq; 
 	pont_arq = fopen("dados.txt", "r");
-	
 	if(pont_arq == NULL){
 		printf("Erro na abertura do arquivo!");
 		return;
 	}
-
 	for(i=1 ; i <= N ; i++)
 		fscanf(pont_arq, "%d, %d\n", &valor[i], &peso[i]);
 	fclose(pont_arq);
-	/*	
-	// populando matriz com valor -1
-	for(i=0;i<MAXN;i++){
-		for(j=0;j<MAXS;j++){
-			t[i][j] = -1;
-		}
-		respostas[i] = 0;
-	}
-	*/
 	return;
 }
 
-void enviar(int remetente, int dest, int *p, int t[N+1][C+1], int b, int i, int size, int rank){
-	if(remetente == 0){
-		t[0][b] = 0;
-
-		return;
-	}
-	if(dest == N + 1){
-		printf("passou aq\n");
+void enviar(int *p, int t[N+1][C+1], int b, int i, int size){
+	if(i == N){
 		return;
 	}
 	int pacote[2];
 	pacote[0] = t[i][b];
-	pacote[1] = -1;
-	if(p[i-1] <= b){
-		pacote[1] = t[i][b-p[i-1]];
+	if(p[i] > b){
+		pacote[1] = -99;
+	}else{
+		pacote[1] = t[i][b-p[i]];
 	}
-	printf("enviar %d COD = %d\n", i, dest);
-	MPI_Send(pacote, 2, MPI_INT, dest%size, i+1, MPI_COMM_WORLD);
+	MPI_Send(pacote, 2, MPI_INT, (i+1)%size, i+1, MPI_COMM_WORLD);
 	return;
 }
 
-void receber(int remetente, int dest, int t[N+1][C+1], int *p, int b, int i, int size, MPI_Status* status){
-	if(remetente == -1){
+void receber(int *p, int t[N+1][C+1], int b, int i, int size, MPI_Status* status){
+	if(i == 0){
+		return;
+	}
+	if(i == 1){
 		t[0][b] = 0;
 		return;
 	}
-	if(dest == 1){
-		return;
-	}
 	int pacote[2];
-	printf("receber %d cod = %d\n", i, remetente);
-	MPI_Recv(pacote, 2, MPI_INT, (remetente)%size, i, MPI_COMM_WORLD, status);
+	MPI_Recv(pacote, 2, MPI_INT, (i-1)%size, i, MPI_COMM_WORLD, status);
 	t[i-1][b] = pacote[0];
-	if (pacote[1] == -1){
-		return;
-	}
 	t[i-1][b-p[i-1]] = pacote[1];
 }
 
@@ -89,9 +68,13 @@ int main(int argc , char *argv[]){
 	int p[N+1];
 	int v[N+1];
 	int t[N+1][C+1];
-	int b,i;
+	int b,i,j;
 
- 	//se for rank 0
+	for(i=0;i<N+1;i++){
+		for(j=0;j<C+1;j++){
+			t[i][j] = 0;
+		}
+	}
 	if(rank == 0){
 		parser(p,v);
 		p[0] =v[0]= 4;
@@ -102,25 +85,40 @@ int main(int argc , char *argv[]){
 	}
 	MPI_Bcast(p, N+1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(v, N+1, MPI_INT, 0, MPI_COMM_WORLD);
- 	//printf("%d\n", p[2]);
-	printf("%d\n", &t[0][0]);
+
 	for(i=rank;i<=n;i = i+size){
-		for(b=0;b<=c;b++){
-			//printf("%d\n", rank);
-			enviar(i, i+1, p, t, b, i, size, rank);
-			
-			receber(i-1, i, t, p, b, i, size, &status);
-			
-			if(p[i-1]>b){
-				t[i][b] = t[i-1][b];
-			}else{
-				t[i][b] = max(t[i-1][b],t[i-1][b-p[i-1]] + v[i-1]);
-			}	
+		if (i != 0){
+			for(b=0;b<=c;b++){
+				
+				receber(p, t, b, i, size, &status);	
+				if(p[i-1]>b){
+					t[i][b] = t[i-1][b];
+				}else{
+					t[i][b] = max(t[i-1][b],t[i-1][b-p[i-1]] + v[i-1]);
+				}
+				enviar(p, t, b, i, size);
+			}
 		}
 	}
 
+//comunicação de volta funcionando corretamente
 	MPI_Barrier(MPI_COMM_WORLD);
+	if(rank != 0){
+		for(i=rank;i<=n;i= i + size){
+			if(i%size != 0){
+				MPI_Send(&t[i][0], C+1, MPI_INT, 0, i, MPI_COMM_WORLD);
+				for (int d = 0; d <= C; ++d){
+				}		
+			}
+		}
+	}
+
 	if(rank == 0){
+		for(i=1;i<=n;i++){
+			if(i%size != 0){
+				MPI_Recv(&t[i][0], C+1, MPI_INT, (i)%size, i, MPI_COMM_WORLD, &status);
+			}
+		}
 		int itens[N+1];
 		b=c;
 		for(i=n; i>0;i--){
@@ -130,9 +128,14 @@ int main(int argc , char *argv[]){
 				printf("%d ... \n",p[i-1]);
 			}
 		}
+		for (int i = 0; i < N+1; ++i){
+			for (j = 0; j < C+1; ++j){
+				printf("%d ",t[i][j] );
+			}
+			printf("\n");
+		}
 	}
- 	//printf("%d rank: %d \n",t[n][c], rank);	
-
+//-------------------------------------------------
 	return 0;
 }
 
---
+++ mochila_ok.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+ //Compiler version gcc 6.3.0
+#define N 4
+#define C 10
+
+
+int max(int x, int y){
+	if(x>y){
+		return x;
+	}
+	return y;
+}
+
+void parser(int* valor, int* peso){
+	int i, j;
+	FILE *pont_arq; 
+	pont_arq = fopen("dados.txt", "r");
+	if(pont_arq == NULL){
+		printf("Erro na abertura do arquivo!");
+		return;
+	}
+	for(i=1 ; i <= N ; i++)
+		fscanf(pont_arq, "%d, %d\n", &valor[i], &peso[i]);
+	fclose(pont_arq);
+	return;
+}
+
+
+int main(int argc , char *argv[]){
+	
+
+	int n = N;
+	int c = C;
+	int p[N+1];
+	int v[N+1];
+	int t[N+1][C+1];
+	int b,i,j;
+
+	for(i=0;i<N+1;i++){
+		for(j=0;j<C+1;j++){
+			t[i][j] = 0;
+		}
+	}
+
+ 	//se for rank 0
+
+	parser(p,v);
+
+	for(i=1;i<=n;i++){
+		for(b=0;b<=c;b++){
+			if(p[i-1]>b){
+				t[i][b] = t[i-1][b];
+			}else{
+				t[i][b] = max(t[i-1][b],t[i-1][b-p[i-1]] + v[i-1]);
+			}	
+		}
+	}
+	
+
+	
+	int itens[N+1];
+	b=c;
+	for(i=n; i>0;i--){
+		if(t[i][b] != t[i-1][b]){
+			itens[i-1]=1;
+			b = b - p[i - 1];
+			printf("%d ... \n",p[i-1]);
+		}
+	}
+	
+	for (int i = 0; i < N+1; ++i)
+	{
+		for (j = 0; j < C+1; ++j)
+		{
+			printf("%d ",t[i][j] );
+		}
+		printf("\n");
+	}
+ 	//printf("%d rank: %d \n",t[n][c], rank);	
+
+	return 0;
+}
+
+ //lamboot 
+ //mpicc mochila_linhas.c -o exe.o && mpirun -np 4 exe.o
